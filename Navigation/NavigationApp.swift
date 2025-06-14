2685052939d995b15a7a81969427f673e7d16572
AntonShiva <ryashinanthon@yandex.ru>
2025-06-14 09:56:23 +0300

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Anton Reasin on 14.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
